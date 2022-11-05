2acef3774358087e4ede5fdd2589cdf41ae92d75
tomas martinez <tomii.jm@hotmail.com>
2022-11-05 00:23:29 -0300

    nueva rama

--- codigo.js
+++ codigo.js
@@ -1,92 +1,35 @@
-let totalCarrito;
-let contenedor = document.getElementById("misprods");
-let botonFinalizar = document.getElementById("finalizar");
-let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-if(carrito.length != 0){
-    console.log("Recuperando carro")
-    dibujarTabla();
-}
+let tienda = document.getElementById("shop");
 
-function dibujarTabla(){
-    for(const producto of carrito){
-        document.getElementById("tablabody").innerHTML += `
-        <tr>
-            <td>${producto.id}</td>
-            <td>${producto.nombre}</td>
-            <td>${producto.precio}</td>
-        </tr>
-    `;
-    }
-    totalCarrito = carrito.reduce((acumulador,producto)=> acumulador + producto.precio,0);
-    let infoTotal = document.getElementById("total");
-    infoTotal.innerText="Total a pagar $: "+totalCarrito;
-}
+let carrito = []
 
-function renderizarProds(){
-    for(const producto of productos){
-        contenedor.innerHTML += `
-            <div class="card col-sm-3">
-                <img src=${producto.foto} class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${producto.id}</h5>
-                    <p class="card-text">${producto.nombre}</p>
-                    <p class="card-text">$ ${producto.precio}</p>
-                    <button id="btn${producto.id}" class="btn btn-primary">Comprar</button>
+let generarTienda = ()=>{ 
+    return (tienda.innerHTML = productos.map((x)=>{
+        let { id, foto, nombre, precio} = x;
+        return `
+        <div id=producto-id-${id} class="item">
+                <img width="221" height="250" src=${foto} alt="">
+                <div class="detalle">
+                    <h3>${nombre}</h3>
+                    <div class="precio-cantidad">
+                        <h3>$${precio}</h3>
+                        <div class="botones">
+                            <i onclick="restar(${id})" class="bi bi-dash-lg"></i>
+                            <div id=${id} class="cantidad">0</div>
+                            <i onclick="sumar(${id})" class="bi bi-plus-lg"></i>
+                        </div>
+                    </div>
                 </div>
             </div>
-        `;
-    }
-
-    //EVENTOS
-    productos.forEach(producto => {
-        //evento para cada boton
-        document.getElementById(`btn${producto.id}`).addEventListener("click",function(){
-            agregarAlCarrito(producto);
-        });
-    })
+        `
+    }).join("")); 
 }
 
-renderizarProds();
+generarTienda();
 
-function agregarAlCarrito(productoComprado){
-    carrito.push(productoComprado);
-    console.table(carrito);
-    Swal.fire({
-        title: productoComprado.nombre,
-        text: 'Agregado al carrito',
-        imageUrl: productoComprado.foto,
-        imageWidth: 200,
-        imageHeight: 200,
-        imageAlt: productoComprado.nombre,
-        showConfirmButton: false,
-        timer: 700
-    })
-    document.getElementById("tablabody").innerHTML += `
-        <tr>
-            <td>${productoComprado.id}</td>
-            <td>${productoComprado.nombre}</td>
-            <td>$${productoComprado.precio}</td>
-        </tr>
-    `;
-    totalCarrito = carrito.reduce((acumulador,producto)=> acumulador + producto.precio,0);
-    let infoTotal = document.getElementById("total");
-    infoTotal.innerText="Total a pagar $: "+totalCarrito;
-    //storage
-    localStorage.setItem("carrito",JSON.stringify(carrito));
-}
-// al clickear finalizar compra se vacia el carrito y toda la tabla
-botonFinalizar.onclick = () => {
-    carrito = [];
-    document.getElementById("tablabody").innerHTML="";
-    let infoTotal = document.getElementById("total");
-    infoTotal.innerText="Total a pagar $: ";
-    Toastify({
-        text: "Pronto recibirá un mail de confirmacion",
-        duration: 3000,
-        gravity: 'bottom',
-        position: 'left',
-        style: {
-            background: 'linear-gradient(to right, #00b09b, #96c92d)'
-        }
-    }).showToast();
-}
+let sumar = (id) => {
+    let itemElegido = id;
+    console.log(itemElegido.id);
+};
+
+let restar = () => {};
+let actualizar = () => {};
--- productos.js
+++ productos.js
@@ -1,24 +1,24 @@
 // Array con los productos
 const productos = [{
-    id: 1,
+    id: "a1",
     foto: "https://allegrastore.com.ar/3278-large_default/combo-almohadones-claudia-crochet-contorno-color-tusor-rosa.jpg",
     nombre: "Almohadones",
     precio: 3500
 },
 {
-    id: 2,
-    foto: "https://d3ugyf2ht6aenh.cloudfront.net/stores/157/089/products/18a1d8c3-25a8-4313-873f-2771dc80482f1-9bbb46d963bf9a18f016527250621708-1024-1024.jpg",
+    id: "b1",
+    foto: "https://images.unsplash.com/photo-1608158222851-af032106bca9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGZyYXphZGF8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
     nombre: "Frazada",
     precio: 4200
 },
 {
-    id: 3,
+    id: "c1",
     foto: "https://mundolosangeles.com.ar/mundo2/wp-content/uploads/2022/05/2.png",
-    nombre: "Cortina",
+    nombre: "Cortinas",
     precio: 3000,
 },
 {
-    id: 4,
+    id: "d4",
     foto: "https://http2.mlstatic.com/D_NQ_NP_835668-MLA45015429205_022021-O.jpg",
     nombre: "Manta",
     precio: 3500
